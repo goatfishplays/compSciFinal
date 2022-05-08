9557b753c779d9ffd14e696ae89b40613802a958
goatfishplays <goatfishplays@gmail.com>
2022-05-08 10:54:19 -0700

    added auto length control of the midsection of long notes

---
+++ CompSciFinal/Assets/longNoteMidCont.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class longNoteMidCont : MonoBehaviour
+{
+    [SerializeField] private Transform tf;
+    private void Awake()
+    {
+        tf = GetComponent<Transform>();
+        tf.localScale = new Vector3(1, tf.parent.localPosition.y*.8f, 1);
+        tf.localPosition = -tf.parent.localPosition/2.0f;
+    }
+}
